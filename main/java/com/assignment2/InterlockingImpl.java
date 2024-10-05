f08d8e5dded53c8b4adc07cec19709906c8c599d
freshinfrancis <freshin.fr@gmail.com>
2024-10-05 12:29:32 +0930

    Update InterlockingImpl.java

--- main/java/com/assignment2/InterlockingImpl.java
+++ main/java/com/assignment2/InterlockingImpl.java
@@ -516,18 +516,4 @@ class Pair<U, V> {
         return "(" + first + ", " + second + ")";
     }
     
-    
-    public static void main(String[] args) {
-        Interlocking network = new InterlockingImpl();
-        network.addTrain("t18",1,8);
-        network.addTrain("t102", 10, 2);
-        network.addTrain("t34", 3, 4);
-        System.out.println(network);
-        network.moveTrains(new String[]{"t18","t34","t102"});
-        System.out.println(network);
-        network.moveTrains(new String[]{"t18","t34","t102"});
-        System.out.println(network);
-        network.moveTrains(new String[]{"t18","t34","t102"});
-        System.out.println(network);
-    }
 }
